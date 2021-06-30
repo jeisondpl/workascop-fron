a406f4700243093fe978beda9c405a7818b3f10b
Jeisondpl <jeisond3@gmail.com>
2021-06-29 23:16:41 -0500

    update

--- src/index.js
+++ src/index.js
@@ -7,8 +7,8 @@ import { ApolloClient, InMemoryCache } from '@apollo/client'
 //cliente para el endpoint
 const client = new ApolloClient({
   connectToDevTools: true,
-  uri: 'http://127.0.0.1:8000/graphql/',
-  //  uri: 'https://workascopback.herokuapp.com/graphql/', //-------------------------
+  // uri: 'http://127.0.0.1:8000/graphql/',
+   uri: 'https://workascopback.herokuapp.com/graphql/', //-------------------------
   // uri: 'http://www.workascop.com/graphql/',
 
   cache: new InMemoryCache(),
